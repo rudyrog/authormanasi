76222bbce509b2b732028ec3a942cbe7a76f8a8d
parv141206 <148616575+parv141206@users.noreply.github.com>
2024-12-17 21:20:56 +0530

    Added transition

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,27 +1,60 @@
 "use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+import { gsap } from "gsap";
+
 export default function Navigation() {
+  const router = useRouter();
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  const handleNavigation = (href: string) => (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    gsap.to(overlayRef.current, {
+      y: "-100%",
+      duration: 2,
+      ease: "power2.inOut",
+      onComplete: () => {
+        router.push(href);
+
+        gsap.to(overlayRef.current, {
+          y: "-200%",
+          duration: 2,
+          ease: "power2.inOut",
+        });
+      },
+    });
+  };
+
   return (
-    <div
-      className="flex items-center justify-center gap-3 text-xl"
-      style={{ color: "wheat" }}
-    >
-      <Link href={"/"} className="navButton">
-        Home
-      </Link>
-      <div>|</div>
-      <Link href={"/books"} className="navButton">
-        Books
-      </Link>
-      <Link href={"/blogs"} className="navButton">
-        Blogs
-      </Link>
-      <Link href={"/about"} className="navButton">
-        About
-      </Link>
-      <Link href={"/contact"} className="navButton">
-        Contact
-      </Link>
-    </div>
+    <>
+      <div
+        ref={overlayRef}
+        className="absolute -top-0 left-[-100vw] z-[50] h-screen w-[200vw] translate-y-[100%] bg-black"
+      ></div>
+
+      <div
+        className="relative z-[51] flex items-center justify-center gap-3 text-xl"
+        style={{ color: "wheat" }}
+      >
+        <Link href="/" onClick={handleNavigation("/")}>
+          Home
+        </Link>
+        <div>|</div>
+        <Link href="/books" onClick={handleNavigation("/books")}>
+          Books
+        </Link>
+        <Link href="/blogs" onClick={handleNavigation("/blogs")}>
+          Blogs
+        </Link>
+        <Link href="/about" onClick={handleNavigation("/about")}>
+          About
+        </Link>
+        <Link href="/contact" onClick={handleNavigation("/contact")}>
+          Contact
+        </Link>
+      </div>
+    </>
   );
 }
