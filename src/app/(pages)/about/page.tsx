a47f409c78acb1df868c4e8f16cc678fb6ab5495
rudyrog <146726039+rudyrog@users.noreply.github.com>
2024-12-19 22:05:00 +0530

    refixed about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -30,56 +30,60 @@ function AboutCard({
 function AboutList() {
   return (
     <div className="about-content fixed left-0 top-0 z-40 h-screen w-[300vw] sm:w-[100vw] md:w-[200vw]">
-      <AboutCard y={20} x={20}>
+      <AboutCard y={30} x={25}>
         <Image
           src="/images/m2Image.jpg"
           alt="manasi"
           style={{ filter: "saturate(0)" }}
-          className="h-96 w-72 rounded-lg sm:h-64 sm:w-48 md:h-72 md:w-56"
-          width={400}
-          height={350}
+          className="rounded-lg sm:h-64 sm:w-48 md:h-96 md:w-72"
+          width={500}
+          height={450}
         />
       </AboutCard>
-      <AboutCard y={-80} x={30}>
+      <AboutCard y={-70} x={40}>
         <div className="relative">
           <Image
             className="rotate-[-6deg]"
             src="/images/paper.png"
             alt="paper"
-            width={500}
+            width={510}
             height={500}
           />
-          <div className="brownbulgary absolute left-10 top-[25vh] w-[30vw] rotate-[-6deg] font-bold text-black sm:w-[90%] md:w-[40vw]">
-            Hi there! I’m Manasi Mehta, a student and part-time blogger from
-            Ahmedabad, Gujarat. Welcome to my blog! I’m excited to share my
-            thoughts and experiences with you, and I hope to spark curiosity and
-            provide fresh perspectives as we explore different topics together.
+          <div className="brownbulgary absolute left-7 top-[25vh] w-[30vw] rotate-[-6deg] text-lg font-bold text-black sm:w-[90%] md:w-[40vw]">
+            Hi there! I’m Manasi Mehta, a student and part-time <br />
+            blogger from Ahmedabad, Gujarat. Welcome to my <br />
+            blog! I’m excited to share my thoughts and <br />
+            experiences with you, and I hope to spark curiosity <br />
+            and provide fresh perspectives as we explore <br />
+            different topics together.
           </div>
         </div>
       </AboutCard>
-      <AboutCard y={-170} x={44}>
+      <AboutCard y={-170} x={60}>
         <div className="relative">
           <Image
             className="rotate-[6deg]"
             src="/images/paper.png"
             alt="paper"
-            width={500}
+            width={525}
             height={500}
           />
-          <div className="brownbulgary absolute left-10 top-[25vh] w-[30vw] rotate-[6deg] font-bold text-black sm:w-[90%] md:w-[40vw]">
-            In my free time, I’m usually writing, cooking, reading, painting, or
-            practicing public speaking. I find joy in the little things and love
-            how creative expression connects us all.
+          <div className="brownbulgary absolute left-10 top-[25vh] w-[30vw] rotate-[6deg] text-lg font-bold text-black sm:w-[90%] md:w-[40vw]">
+            In my free time, I’m usually writing, cooking, reading,
+            <br /> painting, or practicing public speaking. I find joy in <br />
+            the little things and love how creative expression connects us all.
           </div>
         </div>
       </AboutCard>
-      <AboutCard y={-280} x={58}>
+      <AboutCard y={-270} x={80}>
         <div className="relative">
-          <Image src="/images/paper.png" alt="paper" width={500} height={500} />
-          <div className="brownbulgary absolute left-10 top-[25vh] w-[30vw] font-bold text-black sm:w-[90%] md:w-[40vw]">
-            Books are my escape, and I’m always diving into a new one. Through
-            my blogs, I aim to encourage others to embrace self-improvement,
-            share valuable life lessons, and celebrate the beauty of diversity.
+          <Image src="/images/paper.png" alt="paper" width={510} height={500} />
+          <div className="brownbulgary absolute left-10 top-[25vh] w-[30vw] text-lg font-bold text-black sm:w-[90%] md:w-[40vw]">
+            Books are my escape, and I’m always diving into a <br />
+            new one. Through my blogs, I aim to encourage <br /> others to
+            embrace self-improvement, share <br />
+            valuable life lessons, and celebrate the beauty <br />
+            of diversity.
           </div>
         </div>
       </AboutCard>
@@ -88,6 +92,7 @@ function AboutList() {
 }
 
 export default function Books() {
+  1;
   useEffect(() => {
     if (typeof window !== "undefined") {
       const isPhone = window.innerWidth <= 768;
