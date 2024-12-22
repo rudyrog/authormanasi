876f0b1413ee3d5aba27db82ea536501eadbf73f
rudyrog <146726039+rudyrog@users.noreply.github.com>
2024-12-22 16:02:20 +0530

    I Dont Even Know

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,61 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
 import Keyboard from "../components/Keyboard";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
   useEffect(() => {
-    setTimeout(() => {
-      gsap.to(".kbm", {
-        y: "-100%",
-        display: "none",
-        duration: 1,
-      });
-      gsap.fromTo(
-        "#mansi",
-        {
-          x: "0",
+    // Check if window is available (i.e., ensure client-side execution)
+    setIsClient(true);
+  }, []);
+
+  // GSAP effect (running animation)
+  useEffect(() => {
+    if (isClient) {
+      setTimeout(() => {
+        gsap.to(".kbm", {
+          y: "-100%",
+          display: "none",
           duration: 1,
-        },
-        {
-          x: "-25%",
-          display: "block",
-          duration: 1,
-          delay: 1,
-        },
-      );
-      gsap.fromTo(
-        "#mehta",
-        {
-          x: "0%",
-          duration: 1,
-        },
-        {
-          x: "10%",
-          display: "block",
-          duration: 1,
-          delay: 1,
-        },
-      );
-    }, 5000);
-  }, [window]);
+        });
+        gsap.fromTo(
+          "#mansi",
+          {
+            x: "0",
+            duration: 1,
+          },
+          {
+            x: "-25%",
+            display: "block",
+            duration: 1,
+            delay: 1,
+          },
+        );
+        gsap.fromTo(
+          "#mehta",
+          {
+            x: "0%",
+            duration: 1,
+          },
+          {
+            x: "10%",
+            display: "block",
+            duration: 1,
+            delay: 1,
+          },
+        );
+      }, 5000);
+    }
+  }, [isClient]);
+
   return (
-    <div className="max-h-screen overflow-hidden">
+    <>
       <div
         style={{ zIndex: 99999 }}
         className="kbm absolute flex h-screen w-screen items-center justify-center bg-orange-200"
@@ -64,6 +75,6 @@ export default function Home() {
           <div className="image absolute left-[55%] top-52 z-0 h-56 w-52"></div>
         </div>
       </section>
-    </div>
+    </>
   );
 }
